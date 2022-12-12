b8510eff2430a5d3fc9ae5f1da32ea6f603d6e42
Kiara Johnson <johns.kia35@gmail.com>
2022-12-12 15:40:11 -0700

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const pokemon = require('./models/pokemon')
 
 const app = express();
 
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
 app.get('/', (req, res) => res.send('Welcome to the Pokemon App!'));
 
-app.get('/pokemon', (req, res) => res.send(pokemon));
+app.get('/pokemon', (req, res) => res.render("Index"));
 
 
 const PORT = 3000
---
+++ views/Index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+  };
+
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>See the Pokemon!</h1>
+      </div>
+    )
+  }
+}
